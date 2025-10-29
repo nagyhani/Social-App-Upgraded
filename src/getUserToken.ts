398348fddcdd3b428828383edc6ca2b8209a8a33
Nagy Hani <nagyhani337@gmail.com>
2025-10-29 06:29:40 +0300

    token

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -3,10 +3,12 @@ import { cookies } from 'next/headers'
 
 
 export default async function getUserToken() {
+
+    const tokenSession = (process.env.NODE_ENV === "production" ? '__Secure-next-auth.session-token' : 'token')
  
     const cookiesData = await cookies()
 
-    const encryptedToken = cookiesData.get("next-auth.session-token")?.value
+    const encryptedToken = cookiesData.get(tokenSession)?.value
 
 const data = await decode({token : encryptedToken , secret: process.env.NEXTAUTH_SECRET!})
 
